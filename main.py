7c3a2b352f44b85d4e4e4e9e0b64426f2a409640
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-04-20 23:34:31 -0300

    feat: Configuring web server in main file

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from api.endpoints import router as api_router
+import uvicorn
 
-app = FastAPI()
-app.include_router(api_router)
+def create_app():
+    app = FastAPI()
+    app.include_router(api_router)
 
-@app.get('/')
-def read_root():
-    return {'Olá': 'Bem-vindo à Weather API!'}
+    @app.get('/')
+    def read_root():
+        return {'Olá': 'Bem-vindo à Weather API!'}
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
